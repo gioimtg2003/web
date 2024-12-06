80b9335c8b1b30d3e05d5ad3971c7f5b5235f7ba
venerazeka <zekavenera1@gmail.com>
2024-12-06 17:39:04 +0100

    added previous,next and seek button

--- frontend/src/components/PlayerContext.jsx
+++ frontend/src/components/PlayerContext.jsx
@@ -31,6 +31,31 @@ const pause=()=>{
     audioRef.current.pause();
     setPlayStatus(false);
 };
+const playWithId=async(id)=>{
+    await setTrack(songsData[id]);
+await audioRef.current.play();
+setPlayStatus(true);
+}
+
+const previous=async()=>{
+    if(track.id>0){
+        await setTrack(songsData[track.id-1]);
+        await audioRef.current.play();
+        setPlayStatus(true);
+    }
+
+}
+const next=async()=>{
+    if(track.id<songsData.length-1){
+        await setTrack(songsData[track.id+1]);
+        await audioRef.current.play();
+        setPlayStatus(true);
+    }
+
+}
+const seekSong=async(e)=>{
+audioRef.current.currentTime=((e.nativeEvent.offsetX/ seekBg.current.offsetWidth)*audioRef.current.duration)
+}
 useEffect(()=>{
     setTimeout(()=>{
         audioRef.current.ontimeupdate=()=>{
@@ -62,6 +87,9 @@ useEffect(()=>{
         setTime,
         play,
         pause,
+        playWithId,
+        previous,next,
+        seekSong
 
     };
     return(
