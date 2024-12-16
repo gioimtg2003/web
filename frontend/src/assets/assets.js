ca64e99c27cc80a47064b9b5947bfd7d8b6e55be
fionaisaa <157630752+fionaisaa@users.noreply.github.com>
2024-12-16 01:44:54 +0100

    imported new img for front

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -20,6 +20,7 @@ import img2 from './img2.jpg'
 import img3 from './img3.jpg'
 import img4 from './img4.jpg'
 import img5 from './img5.jpg'
+import AM from './AM.jpg'
 import song1 from './song1.mp3'
 import song2 from './song2.mp3'
 import song3 from './song3.mp3'
@@ -79,6 +80,13 @@ export const albumsData =[
       image: img8,
       desc:"Your weekly update of the most played tracks",
       bgColor:"#2a4365" 
+    },
+    {
+    id:5,
+    name: "AM",
+    image: AM,
+    desc:"2013-Album",
+    bgColor:"#2a4375" 
     }
     
   ]
