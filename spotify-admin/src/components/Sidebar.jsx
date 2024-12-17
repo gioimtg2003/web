2a9d60c8c8912ed172180a9834dce78326c303cf
Engjell Berisha <engjell9berisha@gmail.com>
2024-12-17 01:26:45 +0100

    admin panel-add album, list songs

--- spotify-admin/src/components/Sidebar.jsx
+++ spotify-admin/src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <div className='bg-[#003A10] min-h-screen pl-[4vw]'>
+    <div className='bg-[#000000] min-h-screen pl-[4vw]'>
       
       <img src={assets.logo} className='mt-5 w-[max(10vw,100px)] hidden sm:block' alt="" />
       <img src={assets.logo_small} className='mt-5 w-[max(5vw,40px)] mr-5 sm:hidden block'  alt=""/>
